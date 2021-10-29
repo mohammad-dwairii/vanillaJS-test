0e39d3894cfdcd2503ce09379996be3557e09d5e
mohammad-upstack <88664470+mohammad-upstack@users.noreply.github.com>
2021-10-29 14:39:52 +0400

    feat: initial commit for consumer and modal components base

---
+++ scripts.js
@@ -0,0 +1,22 @@
+var btn = document.getElementById("clickBtn");
+
+var modal = document.getElementById("confirmationModal");
+var modalTitle = document.getElementById("confirmationTitle");
+
+btn.onclick = function () {
+  modalTitle.innerText = btn.getAttribute("message");
+  modal.style.display = "block";
+}
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+var resultDiv = document.getElementById("resultDiv");
+
+function onButtonClick(targetButton) {
+  resultDiv.innerHTML = targetButton.getAttribute('message');
+  modal.style.display = "none";
+}
